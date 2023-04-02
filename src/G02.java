03a49cbf7fff943c248f9a18ae44e2135c3671e2
Ömer Ünal <mrfrknl3232@gmail.com>
2023-04-02 13:42:39 +0200

    4.gun

---
+++ src/G02.java
@@ -0,0 +1,19 @@
+public class G02 {
+
+    /*
+            Git-GitHub`da projelerimizin uzerinde calisirken temelde 3 adimda islem yapiyoruz
+                1. Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz
+                        git init
+
+                2. Projemizde / dosyamizda bulunanlari versiyon olusturuyorduk
+                        git add .
+                        git commit -m"versiyonAdi"
+
+                3. gitHub'a yollama/entegrasyonu
+                        git remote add origin repoUrlAdresi
+                        already origin  (hata verirse)
+                        git remote sel-url repoUrlAdresi
+
+                        git push
+         */
+}
